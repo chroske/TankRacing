930c711992066119658e12796890ab13ce0680a5
unknown <chro96ske@gmail.com>
2015-07-11 22:20:24 +0900

    first commit

---
+++ Assets/scripts/invisibleCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class invisibleCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// マウスカーソルを削除する
+		Cursor.visible = false;
+		// マウスカーソルを画面内にロックする
+		Screen.lockCursor = true;
+	}
+}
---
+++ Assets/scripts/shot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public GameObject bullet;
+	public Transform spawn;
+	public float speed = 1000;
+
+	public Transform rifle; //修正箇所
+	private float time = 0f;    //経過時間
+	public float interval = 0.3f;   //何秒おきに発砲するか
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime; //経過時間を加算
+
+		if(Input.GetButton("Fire1")){
+			if(time >= interval){
+				Shoot();    //発砲
+				time = 0f;  //初期化
+			}
+		}
+	}
+	
+	void Shoot () {
+		GameObject obj = GameObject.Instantiate(bullet)as GameObject;
+		obj.transform.position = spawn.position;
+		Vector3 force;
+		force = this.gameObject.transform.forward * speed;
+		obj.GetComponent<Rigidbody>().AddForce (force);
+	}
+}

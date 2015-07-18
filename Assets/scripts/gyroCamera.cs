f7d1a9878e4e7d4a990ebbc6c5bc0ce49f1bf518
unknown <chro96ske@gmail.com>
2015-07-18 17:48:59 +0900

    add gyro

--- Assets/scripts/gyroCamera.cs
+++ Assets/scripts/gyroCamera.cs
@@ -10,6 +10,19 @@ public class gyroCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.AngleAxis(90.0f,Vector3.right)*Input.gyro.attitude*Quaternion.AngleAxis(180.0f,Vector3.forward);
+		//Quaternion gyro = Input.gyro.attitude;
+		
+		//float pitch = Mathf.Asin (2 * gyro.w * gyro.y - 2 * gyro.x * gyro.z);
+		//float roll = Mathf.Atan2 (2 * gyro.y * gyro.z + 2 * gyro.w * gyro.x, -gyro.w*gyro.w + gyro.x*gyro.x + gyro.y*gyro.y - gyro.z*gyro.z);
+		
+		//Vector3 angle = new Vector3(roll*Mathf.Rad2Deg,0,0);
+		
+		//transform.localRotation = Quaternion.Euler(angle) * Quaternion.Euler(270f, 0, 0);
+
+		Quaternion q = Input.gyro.attitude;
+		Quaternion qq = Quaternion.AngleAxis(-90.0f, Vector3.left);
+		q.x *= -1.0f;
+		q.y *= -1.0f;
+		transform.localRotation = qq * q;
 	}
 }
